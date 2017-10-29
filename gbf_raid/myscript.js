998bbe7543ba03fbd487e59b2a1f43edd0cdab2b
Shuyi Qiu <shuyi56@gmail.com>
2017-10-28 23:16:20 -0400

    spelling error

--- gbf_raid/myscript.js
+++ gbf_raid/myscript.js
@@ -16,7 +16,7 @@ var done = false;
 // set interval to make request for boss hp
 
 // get % and if fails to a certain threshold give warning for incoming trigger
-console.log(doucment.title);
+console.log(document.title.indexOf("Granblue Fantasy"));
 if (document.title.indexOf("Granblue Fantasy") != -1) {
   $(document).ready(function() {
     console.log("JQUERY LOADED");
